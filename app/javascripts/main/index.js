176ee961c17149c690a0799bc6a828fb2fd14949
William Blythe <wbwilliam7@gmail.com>
2016-06-03 12:12:25 +1000

    prettifying code

--- app/javascripts/main/index.js
+++ app/javascripts/main/index.js
@@ -6,28 +6,28 @@ json = require('../../package.json');
 electron = require('electron');
 
 electron.app.on('ready', function() {
-  var window;
+    var window;
 
-  window = new electron.BrowserWindow({
-    title: json.name,
-    //width: 800,
-    //height: 600
-    useContentSize: true
-  });
-
-  window.loadURL('file://' + path.join(__dirname, '..', '..') + '/index.html');
-
-  window.webContents.on('did-finish-load', function(){
-    window.webContents.send('loaded', {
-      appName: json.name,
-      electronVersion: process.versions.electron,
-      nodeVersion: process.versions.node,
-      chromiumVersion: process.versions.chrome
+    window = new electron.BrowserWindow({
+        title: json.name,
+        //width: 800,
+        //height: 600
+        useContentSize: true
     });
-  });
 
-  window.on('closed', function() {
-    window = null;
-  });
+    window.loadURL('file://' + path.join(__dirname, '..', '..') + '/index.html');
+
+    window.webContents.on('did-finish-load', function() {
+        window.webContents.send('loaded', {
+            appName: json.name,
+            electronVersion: process.versions.electron,
+            nodeVersion: process.versions.node,
+            chromiumVersion: process.versions.chrome
+        });
+    });
+
+    window.on('closed', function() {
+        window = null;
+    });
 
 });
